954bd2c2bbe8ae7f8313837976eb9fd6b3e5b5bf
IplayLF2 <914637118@qq.com>
2018-10-15 15:01:02 +0800

    优化demo

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -13,17 +13,17 @@ namespace Demo
                 Name = "小明",
                 Character = new[] { Character.幽默, Character.开朗 }
             };
-            Byte[] bytes = BitConverterEX.LittleEndian.GetBytes(person);
-            Int32 index = 0;
-            Person newPerson = BitConverterEX.LittleEndian.ToObject<Person>(bytes, ref index);
+            Byte[] bytes_1 = BitConverterEX.LittleEndian.GetBytes(person);
+            Int32 index_1 = 0;
+            Person newPerson = BitConverterEX.LittleEndian.ToObject<Person>(bytes_1, ref index_1);
 
             Mass mass = new Mass
             {
                 people = new[] { person }
             };
-            bytes = BitConverterEX.LittleEndian.GetBytes(mass);
-            index = 0;
-            Mass newMass = BitConverterEX.LittleEndian.ToObject<Mass>(bytes, ref index);
+            Byte[] bytes_2 = BitConverterEX.LittleEndian.GetBytes(mass);
+            Int32 index_2 = 0;
+            Mass newMass = BitConverterEX.LittleEndian.ToObject<Mass>(bytes_2, ref index_2);
         }
     }
 }
